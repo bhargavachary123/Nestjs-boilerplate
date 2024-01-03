9d7bb06f7cc788135afe19a3b8a4f38a8516c006
Bhargava chary <bhargavacharybasangari@gmail.com>
2024-01-03 18:15:11 +0530

    seeder codes disabled

--- src/database/factories/college.factory.ts
+++ src/database/factories/college.factory.ts
@@ -1,9 +1,9 @@
-import * as Faker from "faker"
-import { define } from "typeorm-seeding"
-import { College } from "src/college/college.entity"
-define(College, (faker: typeof Faker) => {
-  const college = new College()
-  college.name = faker.random.word(); 
-  college.code = faker.random.word();
-  return college;
-})
+// import * as Faker from "faker"
+// import { define } from "typeorm-seeding"
+// import { College } from "src/college/college.entity"
+// define(College, (faker: typeof Faker) => {
+//   const college = new College()
+//   college.name = faker.random.word(); 
+//   college.code = faker.random.word();
+//   return college;
+// })
--- src/database/factories/usermaster.factory.ts
+++ src/database/factories/usermaster.factory.ts
@@ -1,19 +1,19 @@
-import { define, factory } from "typeorm-seeding"
-import * as Faker from "faker"
-import { RType, UserMaster } from "src/usermaster/user-master.entity"
-import { hash } from 'bcrypt';
-import { College } from "src/college/college.entity";
-var generator = require('generate-password');
+// import { define, factory } from "typeorm-seeding"
+// import * as Faker from "faker"
+// import { RType, UserMaster } from "src/usermaster/user-master.entity"
+// import { hash } from 'bcrypt';
+// import { College } from "src/college/college.entity";
+// var generator = require('generate-password');
 
-define(UserMaster, (faker: typeof Faker) => {
-  const username = faker.name.firstName(1);
-  const password = generator.generate({ length: 8, numbers: true, excludeSimilarCharacters: true, strict: true });
-  console.log("\n\n\nyour username:- ", username);
-  console.log("your password:- ", password,"\n\n\n")
-  const userMaster = new UserMaster();
-  userMaster.username = username;
-  userMaster.password = hash(password.trim(), 10);
-  userMaster.role = RType.ADMIN;
-  userMaster.college = factory(College)() as any
-  return userMaster;
-})
+// define(UserMaster, (faker: typeof Faker) => {
+//   const username = faker.name.firstName(1);
+//   const password = generator.generate({ length: 8, numbers: true, excludeSimilarCharacters: true, strict: true });
+//   console.log("\n\n\nyour username:- ", username);
+//   console.log("your password:- ", password,"\n\n\n")
+//   const userMaster = new UserMaster();
+//   userMaster.username = username;
+//   userMaster.password = hash(password.trim(), 10);
+//   userMaster.role = RType.ADMIN;
+//   userMaster.college = factory(College)() as any
+//   return userMaster;
+// })
