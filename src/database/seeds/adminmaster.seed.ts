696f24c7c50374faecaef5e6a3b0418e08cc791c
bhargava chary <bhargavacharybasangari@gmail.com>
2024-03-03 16:55:41 +0530

    seed and factory files removed

--- src/database/seeds/adminmaster.seed.ts
+++
@@ -1,8 +0,0 @@
-// import { Factory, Seeder } from "typeorm-seeding"
-// import { AdminMaster } from "src/adminmaster/admin-master.entity"
-
-// export class CreateAdminMaster implements Seeder {
-//   public async run(factory: Factory): Promise<void> {
-//     await factory(AdminMaster)().create()
-//   }
-// }
